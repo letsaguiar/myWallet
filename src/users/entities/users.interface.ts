7fdcc7b4220542019d3ba1fb2a902d77ab79afa3
Letícia Aguiar <letsaguiar@yandex.com>
2022-09-11 19:05:04 -0300

    fix: users.interface

--- src/users/entities/users.interface.ts
+++ src/users/entities/users.interface.ts
@@ -5,12 +5,12 @@ export interface UserControllerInterface {
   createUser(body: CreateUserDTO): Promise<UserEntity>;
   updateUser(params: UserIdDTO, body: UpdateUserDTO): Promise<UserEntity>;
   getUser(params: UserIdDTO): Promise<UserEntity>;
-  deleteUser(params: UserIdDTO): Promise<UserEntity>;
+  deleteUser(params: UserIdDTO): Promise<void>;
 }
 
 export interface UserServiceInterface {
   createUser(user: CreateUserDTO): Promise<UserEntity>;
   updateUser(userId: number, user: UpdateUserDTO): Promise<UserEntity>;
   getUser(userId: number): Promise<UserEntity>;
-  deleteUser(userId: number): Promise<UserEntity>;
+  deleteUser(userId: number): Promise<void>;
 }
