ff09fe45637d06e1e974f8d56e7f98e66fc75135
Letícia Aguiar <letsaguiar@yandex.com>
2022-09-11 19:01:58 -0300

    fix: users.controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,34 +7,36 @@ import {
   Patch,
   Post,
 } from '@nestjs/common';
+
 import { CreateUserDTO, UpdateUserDTO, UserIdDTO } from './entities/users.dto';
 import { UserEntity } from './entities/users.entities';
+import { UserControllerInterface } from './entities/users.interface';
+
 import { UserService } from './users.service';
 
 @Controller('user')
-export class UserController {
-  constructor(private readonly userService: UserService) {}
+export class UserController implements UserControllerInterface {
+  constructor(
+    private readonly userService: UserService
+  ) {}
 
   @Post()
-  public async create(@Body() body: CreateUserDTO): Promise<UserEntity> {
+  public async createUser(@Body() body: CreateUserDTO): Promise<UserEntity> {
     return this.userService.createUser(body);
   }
 
   @Patch(':user_id')
-  public async update(
-    @Param() params: UserIdDTO,
-    @Body() body: UpdateUserDTO,
-  ): Promise<UserEntity> {
-    return this.userService.updateUser(params, body);
+  public async updateUser(@Param() params: UserIdDTO, @Body() body: UpdateUserDTO): Promise<UserEntity> {
+    return this.userService.updateUser(params.user_id, body);
   }
 
   @Get(':user_id')
-  public async findOne(@Param() params: UserIdDTO): Promise<UserEntity> {
-    return this.userService.findUserById(params);
+  public async getUser(@Param() params: UserIdDTO): Promise<UserEntity> {
+    return this.userService.getUser(params.user_id);
   }
 
   @Delete(':user_id')
-  public async delete(@Param() params: UserIdDTO): Promise<void> {
-    return this.userService.deleteUser(params);
+  public async deleteUser(@Param() params: UserIdDTO): Promise<UserEntity> {
+    return this.userService.deleteUser(params.user_id);
   }
 }
