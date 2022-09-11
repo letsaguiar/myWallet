6ea7b45a386dfc23839b25a65a7ccfbd54eac4a7
Letícia Aguiar <letsaguiar@yandex.com>
2022-09-11 19:01:39 -0300

    feat: create users.interface

---
+++ src/users/entities/users.interface.ts
@@ -0,0 +1,16 @@
+import { CreateUserDTO, UpdateUserDTO, UserIdDTO } from './users.dto';
+import { UserEntity } from './users.entities';
+
+export interface UserControllerInterface {
+  createUser(body: CreateUserDTO): Promise<UserEntity>;
+  updateUser(params: UserIdDTO, body: UpdateUserDTO): Promise<UserEntity>;
+  getUser(params: UserIdDTO): Promise<UserEntity>;
+  deleteUser(params: UserIdDTO): Promise<UserEntity>;
+}
+
+export interface UserServiceInterface {
+  createUser(user: CreateUserDTO): Promise<UserEntity>;
+  updateUser(userId: number, user: UpdateUserDTO): Promise<UserEntity>;
+  getUser(userId: number): Promise<UserEntity>;
+  deleteUser(userId: number): Promise<UserEntity>;
+}
