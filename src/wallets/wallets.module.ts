27c58e4ffc242b68757f8838ba762d064a37415c
Letícia Aguiar <letsaguiar@yandex.com>
2022-09-17 14:42:02 -0300

    feat: import user service into wallets module (MYW-22)

--- src/wallets/wallets.module.ts
+++ src/wallets/wallets.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserService } from '../users/users.service';
 import { WalletEntity } from './entities/wallets.entities';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([WalletEntity])],
+  imports: [
+    TypeOrmModule.forFeature([WalletEntity]),
+    UserService,
+  ],
 })
 export class WalletModule {}
