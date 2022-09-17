3fef83af9e09a93cf5a17fa76a4dcbf03ac83916
Letícia Aguiar <letsaguiar@yandex.com>
2022-09-17 15:12:48 -0300

    feat: create wallets controller (MYW-22)

--- src/wallets/wallets.module.ts
+++ src/wallets/wallets.module.ts
@@ -6,11 +6,14 @@ import { WalletEntity } from './entities/wallets.entities';
 import { UserService } from '../users/users.service';
 import { WalletService } from './wallets.service';
 
+import { WalletController } from './wallets.controller';
+
 @Module({
   imports: [
     TypeOrmModule.forFeature([WalletEntity]),
     UserService,
   ],
   providers: [WalletService],
+  controllers: [WalletController]
 })
 export class WalletModule {}
