758ec609a982a008acf592672f3ba35519f0f8b7
Letícia Aguiar <letsaguiar@yandex.com>
2022-09-18 08:17:00 -0300

    fix: wallets dto

--- src/wallets/entities/wallets.dto.ts
+++ src/wallets/entities/wallets.dto.ts
@@ -1,8 +1,10 @@
-import { OmitType, PartialType } from "@nestjs/swagger";
-import { IsDecimal, IsInt, IsNotEmpty, IsObject, IsString } from "class-validator";
+import { OmitType } from "@nestjs/swagger";
+import { IsDecimal, IsInt, IsNotEmpty, IsObject, IsOptional, IsString } from "class-validator";
 import { UserEntity } from "../../users/entities/users.entities";
+import { Type } from "class-transformer";
 
 export class WalletIdDTO {
+  @Type(() => Number)
   @IsInt()
   wallet_id: number;
 }
@@ -12,23 +14,46 @@ export class CreateWalletDTO {
   @IsNotEmpty()
   name: string;
 
+  @IsOptional()
   @IsString()
   @IsNotEmpty()
-  description: string;
+  description?: string;
 
   @IsDecimal()
   initial_amount: number;
 
+  @Type(() => Number)
   @IsInt()
   user_id: number;
 }
 
-export class UpdateWalletPartialDTO extends PartialType(CreateWalletDTO) {};
+export class UpdateWalletPartialDTO {
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  name?: string;
+
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  description?: string;
+
+  @IsOptional()
+  @IsDecimal()
+  initial_amount?: number;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  user_id?: number;
+};
 
 export class UpdateWalletFullDTO extends OmitType(UpdateWalletPartialDTO, ['user_id'] as const) {
+  @IsOptional()
   @IsDecimal()
   current_balance?: number;
 
+  @IsOptional()
   @IsObject()
   user?: UserEntity;
 }
