8cf6fa188b131584d8233f291b637740aaa18554
Letícia Aguiar <letsaguiar@yandex.com>
2022-09-19 20:20:00 -0300

    fix: module imports

--- src/transactions/transactions.module.ts
+++ src/transactions/transactions.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { WalletModule } from '../wallets/wallets.module';
 import { TransactionEntity } from './entities/transactions.entity';
+import { TransactionController } from './transactions.controller';
+import { TransactionService } from './transactions.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TransactionEntity])],
+  imports: [
+    TypeOrmModule.forFeature([TransactionEntity]),
+    WalletModule,
+  ],
+  exports: [ TransactionService ],
+  providers: [ TransactionService ],
+  controllers: [ TransactionController ],
 })
 export class TransactionModule {}
