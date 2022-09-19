a7d1880333a0541f6596896b680ecf3b4671a9a0
Letícia Aguiar <letsaguiar@yandex.com>
2022-09-19 19:35:41 -0300

    feat: delete outdated features

--- mocks/utilities/generate-id.ts
+++
@@ -1,4 +0,0 @@
-export function generateId(arr: any[]): number {
-    const lastId = arr[arr.length - 1].id;
-    return lastId + 1;
-}
--- mocks/utilities/get-mock.ts
+++
@@ -1,6 +0,0 @@
-import { readFileSync } from "fs";
-import { join } from "path";
-
-export function getMock(path: string): any[] {
-  return JSON.parse(readFileSync(join(process.cwd(), `mocks/${path}`), "utf8"));
-}
